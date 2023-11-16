39c154a2a5d75520e3a0ffb0d6a62de1830f549e
mwanagenzi <njorogemosesngure@gmail.com>
2023-11-16 16:55:56 +0300

    refactor: Updating dependencies and code

--- lib/theme/cesaam_theme.dart
+++ lib/theme/cesaam_theme.dart
@@ -1,21 +1,21 @@
 import 'package:ceesam_app/theme/color_palette.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class CESAAMTheme {
   static ThemeData themeData() {
     return ThemeData(
-    primaryColor: ColorPalette.primaryColor,
-    accentColor: ColorPalette.secondaryColor,
-        appBarTheme: AppBarTheme(
-          backgroundColor: ColorPalette.primaryColor,
-          elevation: 1.0,
-          titleTextStyle:
-              const TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-          centerTitle: true,
+      colorScheme: ColorScheme.fromSwatch().copyWith(
+        primary: ColorPalette.primaryColor,
+        secondary: ColorPalette.secondaryColor,
+      ),
+      appBarTheme: AppBarTheme(
+        backgroundColor: ColorPalette.primaryColor,
+        elevation: 1.0,
+        titleTextStyle: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        centerTitle: true,
       ),
       dividerColor: ColorPalette.secondaryColor,
       //TODO: Configure google fonts for app-wide use
