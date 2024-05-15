91c2e171b1427106edb6ee4909a2b7b274c687bf
mwanagenzi <njorogemosesngure@gmail.com>
2024-05-15 22:31:38 +0300

    refact: Modify network_helper

--- lib/services/network_helper.dart
+++ lib/services/network_helper.dart
@@ -1,7 +1,8 @@
+import 'package:ceesam_app/models/feedback.dart';
+
 class NetworkHelper {
-  String sendFeedback(Feedback feedback) {
-    
+  Future<Map<String, dynamic>> sendFeedback(Feedback feedback) async {
     //todo: make http post calls to the url
-    return '';
+    return {};
   }
 }
